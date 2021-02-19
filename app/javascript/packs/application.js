6bc431079fc1f30b55aa726ae962046b03e68cdf
Marco Barrera <marcobarrera749@gmail.com>
2021-02-18 20:46:07 -0800

    Add select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'bootstrap';
+import improveDropdown from '../components/select';
+
+improveDropdown();
